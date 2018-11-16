22bdb7d78ebcb07353e9ed581cc0de477bf41ba8
Scott Cummins (Ferby) <scottcummins3356@gmail.com>
2018-11-16 10:08:34 +0000

    Version one

--- js/script.js
+++ js/script.js
@@ -6,6 +6,33 @@ stream.stop = () => {
 	clearInterval(stream._interval);
 };
 
+/*
+stream.start = time => {
+
+	stream.timeFormatted = [time.split(":")[0], time.split(":")[1]];
+
+	stream.countdown = () => countdown(null, new Date().setHours(stream.timeFormatted[0], stream.timeFormatted[1], 0));
+
+	stream._interval = setInterval(
+		() => {
+
+			if(stream.countdown().value > 0) {
+				$("#stream-begins").html("Stream begins in <span id='stream-begins__countdown'>" + stream.countdown().toString() + "</span>");
+			}
+
+			else {
+				$("#stream-begins").html("Stream begins in <span id='stream-begins__countdown'>0 seconds</span>");
+				stream.stop();
+				console.log("Countdown stopped, time has passed");
+			}
+
+		},
+		500
+	)
+
+};
+ */
+
 stream.start = time => {
 
 	stream.timeFormatted = [time.split(":")[0], time.split(":")[1]];
